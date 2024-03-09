c1989da2a58dcb40ae3cabc00809e28fa0062168
mnicolas94 <miguelcok27@gmail.com>
2024-03-08 20:44:19 -0500

    chore(ad break): add property accessors to CrazyGamesAdBreak's events

--- Runtime/CrazySDKUtils/CrazyGamesAdBreak.cs
+++ Runtime/CrazySDKUtils/CrazyGamesAdBreak.cs
@@ -7,11 +7,17 @@ namespace CrazyGamesUtils.CrazySDKUtils
     public class CrazyGamesAdBreak : MonoBehaviour
     {
         [SerializeField] private UnityEvent _onAdBreakCompleted;
+        public UnityEvent OnAdBreakCompleted => _onAdBreakCompleted;
+        
         [SerializeField] private UnityEvent _onAdBreakError;
+        public UnityEvent OnAdBreakError => _onAdBreakError;
         
         [SerializeField] private UnityEvent _onAdBreakRewardedCompleted;
-        [SerializeField] private UnityEvent _onAdBreakRewardedError;
+        public UnityEvent OnAdBreakRewardedCompleted => _onAdBreakRewardedCompleted;
         
+        [SerializeField] private UnityEvent _onAdBreakRewardedError;
+        public UnityEvent OnAdBreakRewardedError => _onAdBreakRewardedError;
+
         public void BeginAdBreak()
         {
             CrazyAds.Instance.beginAdBreak(
